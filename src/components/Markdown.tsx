5738a395fad92c172d5211d6c345094b75afa586
MadDogOwner <xiaoran@xrgzs.top>
2024-12-27 21:15:45 +0800

    feat: markdown mermaid support (#214)

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -162,6 +162,13 @@ const insertKatexCSS = once(() => {
   document.head.appendChild(link)
 })
 
+const insertMermaidJS = once(() => {
+  const script = document.createElement("script")
+  script.src =
+    "https://registry.npmmirror.com/mermaid/11/files/dist/mermaid.min.js"
+  document.body.appendChild(script)
+})
+
 export function Markdown(props: {
   children?: string | ArrayBuffer
   class?: string
@@ -213,9 +220,14 @@ export function Markdown(props: {
     on(md, () => {
       setShow(false)
       insertKatexCSS()
+      insertMermaidJS()
       setTimeout(() => {
         setShow(true)
         hljs.highlightAll()
+        window.mermaid &&
+          window.mermaid.run({
+            querySelector: ".language-mermaid",
+          })
         window.onMDRender && window.onMDRender()
       })
     }),
