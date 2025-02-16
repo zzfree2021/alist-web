08e59d9d7f984233066dd286306f52b9b2dc30a5
KirCute_ECT <951206789@qq.com>
2025-02-16 12:24:31 +0800

    feat(archive): support deprioritize previewing (#252)

--- src/pages/home/previews/index.ts
+++ src/pages/home/previews/index.ts
@@ -1,5 +1,5 @@
 import { Component, lazy } from "solid-js"
-import { getIframePreviews, me } from "~/store"
+import { getIframePreviews, me, getSettingBool } from "~/store"
 import { Obj, ObjType, UserMethods, UserPermissions } from "~/types"
 import { ext } from "~/utils"
 import { generateIframePreview } from "./iframe"
@@ -7,6 +7,7 @@ import { useRouter } from "~/hooks"
 import { getArchiveExtensions } from "~/store/archive"
 
 type Ext = string[] | "*" | (() => string[])
+type Prior = boolean | (() => boolean)
 
 const extsContains = (exts: Ext | undefined, ext: string): boolean => {
   if (exts === undefined) {
@@ -20,12 +21,20 @@ const extsContains = (exts: Ext | undefined, ext: string): boolean => {
   }
 }
 
+const isPrior = (p: Prior): boolean => {
+  if (typeof p === "boolean") {
+    return p
+  }
+  return p()
+}
+
 export interface Preview {
   name: string
   type?: ObjType
   exts?: Ext
   provider?: RegExp
   component: Component
+  prior: Prior
 }
 
 export type PreviewComponent = Pick<Preview, "name" | "component">
@@ -35,74 +44,88 @@ const previews: Preview[] = [
     name: "HTML render",
     exts: ["html"],
     component: lazy(() => import("./html")),
+    prior: true,
   },
   {
     name: "Aliyun Video Previewer",
     type: ObjType.VIDEO,
     provider: /^Aliyundrive(Open)?$/,
     component: lazy(() => import("./aliyun_video")),
+    prior: true,
   },
   {
     name: "Markdown",
     type: ObjType.TEXT,
     component: lazy(() => import("./markdown")),
+    prior: true,
   },
   {
     name: "Markdown with word wrap",
     type: ObjType.TEXT,
     component: lazy(() => import("./markdown_with_word_wrap")),
+    prior: true,
   },
   {
     name: "Url Open",
     exts: ["url"],
     component: lazy(() => import("./url")),
+    prior: true,
   },
   {
     name: "Text Editor",
     type: ObjType.TEXT,
     exts: ["url"],
     component: lazy(() => import("./text-editor")),
+    prior: true,
   },
   {
     name: "Image",
     type: ObjType.IMAGE,
     component: lazy(() => import("./image")),
+    prior: true,
   },
   {
     name: "Video",
     type: ObjType.VIDEO,
     component: lazy(() => import("./video")),
+    prior: true,
   },
   {
     name: "Audio",
     type: ObjType.AUDIO,
     component: lazy(() => import("./audio")),
+    prior: true,
   },
   {
     name: "Ipa",
     exts: ["ipa", "tipa"],
     component: lazy(() => import("./ipa")),
+    prior: true,
   },
   {
     name: "Plist",
     exts: ["plist"],
     component: lazy(() => import("./plist")),
+    prior: true,
   },
   {
     name: "Aliyun Office Previewer",
     exts: ["doc", "docx", "ppt", "pptx", "xls", "xlsx", "pdf"],
     provider: /^Aliyundrive(Share)?$/,
     component: lazy(() => import("./aliyun_office")),
+    prior: true,
   },
   {
     name: "Asciinema",
     exts: ["cast"],
     component: lazy(() => import("./asciinema")),
+    prior: true,
   },
   {
     name: "Video360",
     type: ObjType.VIDEO,
     component: lazy(() => import("./video360")),
+    prior: true,
   },
   {
     name: "Archive Preview",
@@ -114,6 +137,7 @@ const previews: Preview[] = [
       return getArchiveExtensions()
     },
     component: lazy(() => import("./archive")),
+    prior: () => getSettingBool("preview_archives_by_default"),
   },
 ]
 
@@ -124,6 +148,7 @@ export const getPreviews = (
   const typeOverride =
     ObjType[searchParams["type"]?.toUpperCase() as keyof typeof ObjType]
   const res: PreviewComponent[] = []
+  const subsequent: PreviewComponent[] = []
   // internal previews
   previews.forEach((preview) => {
     if (preview.provider && !preview.provider.test(file.provider)) {
@@ -134,7 +159,12 @@ export const getPreviews = (
       (typeOverride && preview.type === typeOverride) ||
       extsContains(preview.exts, ext(file.name).toLowerCase())
     ) {
-      res.push({ name: preview.name, component: preview.component })
+      const r = { name: preview.name, component: preview.component }
+      if (isPrior(preview.prior)) {
+        res.push(r)
+      } else {
+        subsequent.push(r)
+      }
     }
   })
   // iframe previews
@@ -150,5 +180,5 @@ export const getPreviews = (
     name: "Download",
     component: lazy(() => import("./download")),
   })
-  return res
+  return res.concat(subsequent)
 }
